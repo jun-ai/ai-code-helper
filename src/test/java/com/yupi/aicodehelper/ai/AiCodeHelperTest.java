2a211792aa66ff88bec595f36afe5861a9f925d6
40027147 <40027147@jack.com>
2025-07-08 12:11:24 +0800

    项目初始化 + 简单对话

---
+++ src/test/java/com/yupi/aicodehelper/ai/AiCodeHelperTest.java
@@ -0,0 +1,17 @@
+package com.yupi.aicodehelper.ai;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperTest {
+
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("你好，我是程序员鱼皮");
+    }
+}
