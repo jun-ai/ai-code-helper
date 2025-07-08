8143d167bd8e88087a21693d3a9040ac480a4324
40027147 <40027147@jack.com>
2025-07-08 17:19:52 +0800

    优化 RAG 代码

--- src/main/java/com/yupi/aicodehelper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/yupi/aicodehelper/ai/AiCodeHelperServiceFactory.java
@@ -30,41 +30,12 @@ public class AiCodeHelperServiceFactory {
     private ChatModel qwenChatModel;
 
     @Resource
-    private EmbeddingModel qwenEmbeddingModel;
-
-    @Resource
-    private EmbeddingStore<TextSegment> embeddingStore;
+    private ContentRetriever contentRetriever;
 
     @Bean
     public AiCodeHelperService aiCodeHelperService() {
         // 会话记忆
         ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
-        // ------ RAG ------
-        // 1. 加载文档
-        List<Document> documents = FileSystemDocumentLoader.loadDocuments("src/main/resources/docs");
-        // 2. 文档切割：将每个文档按每段进行分割，最大 1000 字符，每次重叠最多 200 个字符
-        DocumentByParagraphSplitter paragraphSplitter = new DocumentByParagraphSplitter(1000, 200);
-        // 3. 自定义文档加载器
-        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .documentSplitter(paragraphSplitter)
-                // 为了提高搜索质量，为每个 TextSegment 添加文档名称
-                .textSegmentTransformer(textSegment -> TextSegment.from(
-                        textSegment.metadata().getString("file_name") + "\n" + textSegment.text(),
-                        textSegment.metadata()
-                ))
-                // 使用指定的向量模型
-                .embeddingModel(qwenEmbeddingModel)
-                .embeddingStore(embeddingStore)
-                .build();
-        // 加载文档
-        ingestor.ingest(documents);
-        // 4. 自定义内容查询器
-        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(embeddingStore)
-                .embeddingModel(qwenEmbeddingModel)
-                .maxResults(5) // 最多 5 个检索结果
-                .minScore(0.75) // 过滤掉分数小于 0.75 的结果
-                .build();
         // 构造 AI Service
         AiCodeHelperService aiCodeHelperService = AiServices.builder(AiCodeHelperService.class)
                 .chatModel(qwenChatModel)
